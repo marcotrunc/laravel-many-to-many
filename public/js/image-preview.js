e43181e3516df8597ab4e2e6e1791ef4cb7f5057
marcotrunc <marcotrunc@gmail.com>
2022-04-01 17:40:34 +0200

    edited the script for the preview

--- public/js/image-preview.js
+++ public/js/image-preview.js
@@ -2,8 +2,12 @@ const imageInput = document.getElementById('image');
 const preview = document.getElementById('preview');
 const placeholder = 'https://www.geometrian.it/wp-content/uploads/2016/12/image-placeholder-500x500.jpg';
 
-imageInput.addEventListener('change', (e) => {
-    const url = imageInput.value;
-    if (url) preview.setAttribute("src", e.target.value);
-    else preview.setAttribute("src", placeholder);
+imageInput.addEventListener('change', () => {
+    if (imageInput.files && imageInput.files[0]) { //non fare il .value perché addesso l'input è di tipo file 
+        let reader = new FileReader();
+        reader.readAsDataURL(imageInput.files[0]); //Crea un Data-Url, è asincrona
+        reader.onload = e => { //Quando finisce di leggere esegue l'arrow function
+            preview.setAttribute('src', e.target.result);
+        };
+    }
 });
